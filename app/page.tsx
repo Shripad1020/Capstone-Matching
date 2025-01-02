5c240bff08b7193bcb13be08e2445496bb9cee41
Shripad Kulkarni <shripad1020@gmail.com>
2025-01-02 03:35:06 -0600

    created homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
 	return (
-		<>
-		<h1>Hello, World!</h1>
-		</>
+		<main className="flex flex-col items-center justify-center h-full">
+			<h2>Get Started</h2>
+			<p className="mb-3">Join our community of students and educators to discover or propose capstone projects.</p>
+			<Button variant="secondary">Login</Button>
+		</main>
 	);
 }
---
+++ components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="text-center">
+            <p>&copy; 2025 Your Company. All Rights Reserved.</p>
+        </footer>
+    );
+}
